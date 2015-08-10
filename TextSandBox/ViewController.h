fdbda4daeb916480c8c91e69addcf2394ba9fe7e
jungthinkr <aaronjli@usc.edu>
2015-08-10 13:35:16 -0700

    Initial Commit

---
+++ TextSandBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextSandBox
+//
+//  Created by Aaron Liu on 8/10/15.
+//  Copyright (c) 2015 Aaron Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TextSandBox/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TextSandBox
+//
+//  Created by Aaron Liu on 8/10/15.
+//  Copyright (c) 2015 Aaron Liu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
