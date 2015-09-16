3388f077378b0b45cacf388ad6043fc35d069bc1
Aaron Liu <aaronliu@Aarons-MacBook-Air.local>
2015-09-16 14:12:38 -0700

    9/16/15
    adsf

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+
+target 'TextSandBox' do
+pod 'pop', '~> 1.0'
+end
+
+target 'TextSandBoxTests' do
+
+end
+
--- TextSandBox/ViewController.m
+++ TextSandBox/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
-
+#import <pop/POP.h>
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *test;
 
 @end
 
@@ -16,6 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+     self.test.delegate = self;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,5 +25,58 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - change size of text field
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    NSString *testfield;
+    if (range.length == 0) {
+        testfield = [NSString stringWithFormat:@"%@%@", textField.text, string];
+    } else {
+        testfield = [textField.text substringToIndex:textField.text.length - range.length];
+    }
+    NSUInteger length = [self getLength:testfield];
+
+    NSLog(@"%lu", length);
+    NSLog(@"%@", testfield);
+    NSLog(@"%lu", range.length);
+
+
+    if ([testfield length] >= 10)
+    {
+        POPBasicAnimation *scaleAnimation = [POPBasicAnimation animationWithPropertyNamed:kPOPViewScaleXY];
+       // scaleAnimation.duration = 0.1;
+        scaleAnimation.toValue = [NSValue valueWithCGPoint:CGPointMake(.3, .3)];
+      //  scaleAnimation.springSpeed= @(100.);
+        [textField pop_addAnimation:scaleAnimation forKey:@"scalingUp"];
+
+       // [textField setFont:[UIFont fontWithName:@"Helvetica" size:20]];
+    }
+    else
+    {
+        POPBasicAnimation *scaleAnimation = [POPBasicAnimation animationWithPropertyNamed:kPOPViewScaleXY];
+        //scaleAnimation.duration = ;
+        scaleAnimation.toValue = [NSValue valueWithCGPoint:CGPointMake(1, 1)];
+        [textField pop_addAnimation:scaleAnimation forKey:@"scalingUp2"];
+    
+    }
+
+    return YES;
+
+}
+
+-(NSInteger)getLength:(NSString*)test
+{
+    
+
+    
+    NSInteger length = [test length];
+    
+    return length;
+    
+    
+}
+
 
 @end
